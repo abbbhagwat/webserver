4d14d0d4f8af292aa81da036579c7c8b121a3745
Amit Bhagwat <abhagwat@cybage.com>
2022-03-28 13:58:48 +0530

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+
+console.log('Client side javascript file is loaded!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#p1')
+const messagetwo = document.querySelector('#p2')
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = ''
+    messagetwo.textContent = ''
+
+  
+    const url = 'http://localhost:3000/weather?address=' + location
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = (data.error)
+            } else {
+                messagetwo.textContent = (data.location) + '\n'+ (data.forecast)
+                
+            }
+        })
+    })
+    
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+const url = '42.3605,-71.0596'
+const forecast  = (latitude, longitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=8fe25c5c2e191a1eacf59f9528dcf893&query='+ latitude + ',' + longitude
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!',undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location',undefined)
+        } else
+        callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+        })
+
+}
+
+module.exports = forecast
